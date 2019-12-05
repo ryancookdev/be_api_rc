b99e3b72dae8d7f1662c8b3e383b9a83267f3feb
Ryan Cook <ryan@ryancook.software>
2019-12-05 12:58:35 -0500

    Create Person domain class

---
+++ src/Domain/Person.py
@@ -0,0 +1,8 @@
+class Person:
+    def __init__(self, id, fname, mname, lname, phone):
+        self.internal_id = id
+        self.first_name = fname
+        self.middle_name = mname
+        self.last_name = lname
+        self.phone = phone
+        
