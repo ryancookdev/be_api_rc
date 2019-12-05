089fb92a674c4a8ad6500fc933439312153fde19
Ryan Cook <ryan@ryancook.software>
2019-12-05 13:00:00 -0500

    Parse CSV row into Person object

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 import argparse
+import csv
 import os
 import time
+from Domain.Person import Person
 
 def main():
     args = get_args()
@@ -8,7 +10,17 @@ def main():
     process_queue = []
     while 1:
         files = [f for f in os.listdir(args.input_dir)]
+        for file in files:
+            process_input_file(os.path.join(args.input_dir, file))
         time.sleep(args.wait)
+
+
+def process_input_file(file):
+    with open(file) as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        next(csv_reader) # skip header
+        for row in csv_reader:
+            person = Person(row[0], row[1], row[2], row[3], row[4])
 
     # TODO: validate input dir exists
     # TODO: validate file ends with .csv
